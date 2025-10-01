2b668ce3023a0dc813bc5814d18dcaff2c273190
chamindu77 <chamindunipun99@gmail.com>
2025-10-01 23:27:58 +0530

    feat: initial FastAPI setup with routing, schemas, DB integration, CRUD ops, and error handling

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 @app.get("/")
 def index():
-    return {'data':{'name':'chamindu'}}
+    return {'data':'blog list'}
 
-@app.get("/about")
-def about():
-    return {'data':{'about page': 'This is the about page'}}
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id:int):
+    return {'data': f'comments for blog {id}'}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    author: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f'blog is created with title {request.title}'}
